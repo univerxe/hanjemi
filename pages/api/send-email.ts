5655e09a60bff30ede6b74908d9b31c5ee3a5527
Universe Shrestha <cssthauni@gmail.com>
2025-03-16 04:46:35 +0900

    Add telegram channel

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const TELEGRAM_BOT_TOKEN = '8131686927:AAFGC5J9yOpiO8Z0WuvGGjtI2xWo0E27dik'
-const TELEGRAM_CHAT_ID = '6015407610'
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
@@ -9,22 +9,32 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const message = `New email submission: ${email}`
 
-    const response = await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        chat_id: TELEGRAM_CHAT_ID,
-        text: message,
-      }),
-    })
+    try {
+      const response = await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          chat_id: TELEGRAM_CHAT_ID,
+          text: message,
+        }),
+      })
 
-    if (response.ok) {
-      res.status(200).json({ message: 'Email sent to Telegram' })
-    } else {
-      const errorData = await response.json()
-      res.status(500).json({ error: 'Failed to send email to Telegram', details: errorData })
+      if (response.ok) {
+        res.status(200).json({ message: 'Email sent to Telegram' })
+      } else {
+        const errorData = await response.json()
+        console.error('Error sending message to Telegram:', errorData)
+        res.status(500).json({ error: 'Failed to send email to Telegram', details: errorData })
+      }
+    } catch (error) {
+      console.error('Error:', error)
+      if (error instanceof Error) {
+        res.status(500).json({ error: 'Failed to send email to Telegram', details: error.message })
+      } else {
+        res.status(500).json({ error: 'Failed to send email to Telegram', details: 'Unknown error' })
+      }
     }
   } else {
     res.status(405).json({ error: 'Method not allowed' })
